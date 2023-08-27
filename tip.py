88413f57921a429ba48e9ce246982abfc06e5ac7
Brian <wamwangimathenge@gmail.com>
2023-08-27 10:59:33 +0300

    Calculate the amounts of an 18% tip

--- tip.py
+++ tip.py
@@ -1,3 +1,7 @@
 # The program should ask the user to enter the charge for the food
-
 charge = input("Charge for food = $")
+print()
+
+# Calculate the amounts of an 18% tip and 7% sales tax on the charge of the food
+tip = float(charge) * 0.18
+print("Tip =", tip)
