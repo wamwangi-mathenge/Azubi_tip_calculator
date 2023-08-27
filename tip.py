d18e03ce4ca772e3ce72274701f9e0f48d46306c
Brian <wamwangimathenge@gmail.com>
2023-08-27 11:16:29 +0300

    Add the $ currency in each value

--- tip.py
+++ tip.py
@@ -5,12 +5,12 @@ print()
 # Calculate the amounts of an 18% tip and 7% sales tax on the charge of the food
 tip = float(charge) * 0.18
 rounded_tip = round(tip, 2)
-print("Tip =", rounded_tip)
+print(f"Tip = ${rounded_tip}")
 print()
 sales_tax = float(charge) * 0.07
 rounded_sales = round(sales_tax, 2)
-print("Sales tax =", rounded_sales)
+print(f"Sales tax = ${rounded_sales}")
 print()
 # Add everything together and display the charge of the food plus tip and sales tax
 grand_total = float(charge) + float(rounded_tip) + float(rounded_sales)
-print("Grand total =", grand_total)
+print(f"Grand total = ${grand_total}")
